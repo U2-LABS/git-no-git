c71b70f27bef45d59e3c0e5b9badbfa1a496b346
NikEfr7 <nikefremov.00.008@hotmail.com>
2020-11-11 23:02:39 +0300

    Add finder of txt file in folder

--- solution_4/no_git_no.py
+++ solution_4/no_git_no.py
@@ -1,5 +1,6 @@
 import pygit2
 import os
+import glob
 
 current_working_directory = os.getcwd()
 repository_path = pygit2.discover_repository(current_working_directory)
@@ -7,15 +8,18 @@ repo = pygit2.Repository(repository_path)
 
 index = repo.index
 
+os.chdir('..')
 # Create test files in repo
 for x in range(1, 4):
-    fname = f'filename{x}'
-
+    os.mkdir(f'folder{x}')
+    fname = f'folder{x}/filename{x}.txt'
     with open(fname, 'w') as f:
         f.write('test_data')
-    index.add(fname) # add file. Replace `git add filename`
-    index.write() # write filename in index
 
+for path in glob.glob(f'**/filename*.txt', recursive=True):
+    index.add(path.replace('\\','/'))# add file. Replace `git add filename`
+
+index.write() # write filename in index
 
 commit = repo[repo.head.target] # get info about last commit
 master = repo.branches.get('master') # get info about our branch master
